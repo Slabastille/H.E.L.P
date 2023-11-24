20d4e0a42539b5be08628bb9c053383367a9c02e
Samuel Labastille <slabastille@SH-XVW0W6PJXF.local>
2023-11-24 15:50:12 -0500

    Added clinician info page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import TicketForm from './TicketForm';
+import ClinicianInfo from './ClinicianInfo';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div id="container">
-      <div className="header">
-        <h1>Mobile Support App *fix header later</h1>
+    <Router>
+      <div id="container">
+        <div className="header">
+          <Link to="/clinicianInfo">
+            <h1>Mobile Support App *fix header later</h1>
+          </Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={ClinicianInfo} />
+          <Route exact path="/clinicianInfo" component={ClinicianInfo} />
+          <Route exact path="/ticketForm" component={TicketForm} />
+        </Switch>
+        {/* WORK ON FOOTER LATER<div className="footer">Footer this was created by Samuel</div> */}
       </div>
-      <div className="tempBody">
-        <TicketForm />
-      </div>
-      <div className="footer">Footer this was created by Samuel</div>
-    </div>
+    </Router>
   );
 };
 
--- src/TicketForm.js
+++ src/TicketForm.js
@@ -3,39 +3,37 @@ import React from 'react';
 const TicketForm = () => {
   return (
     <div className="ticketFormPage">
-      <div className="main-block">
-        <form className="ticketForm" action="/">
-          <h1 className="formHeader">Enter Ticket Information</h1>
+      <form className="ticketForm" action="/">
+        <h1 className="formHeader">Enter Ticket Information</h1>
 
-          <div className="formBody">
-            <input
-              className="summary"
-              type="text"
-              name="name"
-              placeholder="Summary"
-            />
+        <div className="formBody">
+          <input
+            className="summary"
+            type="text"
+            name="name"
+            placeholder="Summary"
+          />
 
-            <select className="selectRequestType">
-              {/* Will need an onchange here to change the selector color later */}
-              <option> Choose a Request Type</option>
-              <option>Support Request</option>
-              <option>Supply Request</option>
-            </select>
+          <select className="selectRequestType">
+            {/* Will need an onchange here to change the selector color later */}
+            <option> Choose a Request Type</option>
+            <option>Support Request</option>
+            <option>Supply Request</option>
+          </select>
 
-            <div>
-              <textarea
-                className="description"
-                placeholder="Description"
-                rows="4"
-              ></textarea>
-            </div>
+          <div>
+            <textarea
+              className="description"
+              placeholder="Description"
+              rows="6"
+            ></textarea>
           </div>
+        </div>
 
-          <button className="TicketFormBtn" type="submit" href="/">
-            Submit
-          </button>
-        </form>
-      </div>
+        <button className="TicketFormBtn" type="submit" href="/">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
