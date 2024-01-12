76a80ff2b7921a2adeb2e24e504a2cd3b5f79562
Brandon <brandonrhodes1630@gmail.com>
2024-01-12 15:34:24 -0600

    Revert "Service Request & Create Issue"
    This reverts commit ba1ae21b7c7b62c010efeecbcc4d17cf7fd3873c.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TicketForm from './TicketForm';
 import ClinicianInfo from './ClinicianInfo';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { HelpProvider } from './context/HelpContext';
 import SupplyForm from './SupplyForm';
-import CreateIssue from './CreateIssue';
-import CreateIssueV2 from './CreateIssueV2';
-import ServiceRequest from './ServiceRequest';
 
 const App = () => {
   return (
     <HelpProvider>
       <Router>
-        <div id="container" >
-          <div className="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', boxShadow: '0 0 10px rgba(0, 0, 0, 1)' }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <img src="/shlogo.PNG" alt="Logo" style={{ width: '50px', marginLeft: '10px', padding: '10px' }} />
-              <h1 style={{ padding: '10px', marginLeft: '10px' }}>IT Works</h1>
-            </div>
-            <img src="/profileimg.PNG" alt="Profile Icon" style={{ width: '50px', marginRight: '10px', padding: '10px' }} />
+        <div id="container">
+          <div className="header">
+            <Link to="/">
+              <h1>Mobile Support App *fix header later</h1>
+            </Link>
           </div>
           <Switch>
-            <Route exact path="/" component={ServiceRequest} />
+            <Route exact path="/" component={ClinicianInfo} />
             <Route exact path="/clinicianInfo" component={ClinicianInfo} />
             <Route exact path="/ticketForm" component={TicketForm} />
             <Route exact path="/supplyForm" component={SupplyForm} />
@@ -35,11 +30,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-
-
-
