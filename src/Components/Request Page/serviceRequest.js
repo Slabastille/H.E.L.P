cca3d2a85d5b42230cdaf346000612b74586ecb2
Samuel Labastille <slabastille@SH-XVW0W6PJXF.local>
2024-04-01 00:24:28 -0400

    updated ticketForm Page

--- src/Components/Request Page/serviceRequest.js
+++ src/Components/Request Page/serviceRequest.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReporterPastTickets from './ReporterPastTickets';
 import ClinicianStatus from './ClinicianStatus';
 import TicketForm from '../CreateIssue Page/TicketForm';
 
-const MSRequestPage = () => (
-  <div className="request-container">
-    <div className="request-left-box">
-      <div className="request-left-box-container">
-        <TicketForm />
+const MSRequestPage = () => {
+  //confirm before leaving page
+  // useEffect(() => {
+  //   const handleBeforeUnload = (e) => {
+  //     e.preventDefault();
+  //     e.returnValue = '';
+  //   };
+
+  //   window.addEventListener('beforeunload', handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener('beforeunload', handleBeforeUnload);
+  //   };
+  // }, []);
+  return (
+    <div className='pageContainer'>
+
+      <div className="request-container">
+        <div className="request-left-box">
+          <div className="request-left-box-container">
+            <TicketForm />
+          </div>
+        </div>
+        {/* Add feature that shows clinician assigned ipads here */}
+        <div className="request-right-box">
+          <div className="request-top-right-box">
+            <ClinicianStatus />
+          </div>
+          <div className="request-bottom-right-box">
+            <ReporterPastTickets />
+          </div>
+        </div>
       </div>
     </div>
-    {/* Add feature that shows clinician assigned ipads here */}
-    <div className="request-right-box">
-      <div className="request-top-right-box">
-        <ClinicianStatus />
-      </div>
-      <div className="request-bottom-right-box">
-        <ReporterPastTickets />
-      </div>
-    </div>
-  </div>
-);
+  )
+} 
 
 export default MSRequestPage;
